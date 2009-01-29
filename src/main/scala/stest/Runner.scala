1d6e08ccb19edf678e3bf4855f3779099b2bdc18
Michael Neale <michael.neale@gmail.com>
2009-01-29 11:59:07 +1100

    some refactoring

--- src/main/scala/stest/Runner.scala
+++ src/main/scala/stest/Runner.scala
@@ -5,11 +5,39 @@ package stest
  */
 
 import java.io.InputStream
-import jxl.Workbook
-
+import jxl.{Sheet, Workbook, Cell}
 class Runner {
-    def doTests() = {
-        42
+
+
+
+    def processSheet(st: Sheet) = {
+              
+          val declarationCells = st.getColumn(0).takeWhile((c: Cell) => c.getContents != "WHEN")
+          val dataCells = st getColumn(0) dropWhile(_.getContents != "WHEN") drop(1) takeWhile(_.getContents != "EXPECT")
+          val expectCells = st.getColumn(0).dropWhile(_.getContents != "EXPECT").drop(1)
+
+
+          val facts = declarationCells.dropWhile(_.getContents.startsWith("Global"))
+          val globals = declarationCells.takeWhile(_.getContents.startsWith("Global"))
+
+
+
+
+          lazy val dataStartRow = dataCells(0).getRow
+          lazy val expectStartRow = expectCells(0).getRow
+
+          //now lets do column 1
+          val scenarioData = st getColumn(1) dropWhile(_.getRow <  dataStartRow) takeWhile(_.getRow < expectStartRow - 1)
+          val expectationData = st getColumn(1) dropWhile(_.getRow < expectStartRow)
+
+
+          val scenario1Input = dataCells.map((c: Cell) => (c.getContents.replace(' ', '.'), scenarioData(c.getRow - dataStartRow).getContents))
+          println(scenario1Input)
+
+          val scenario1Expectations = expectCells.map((c: Cell) => ((c.getContents.replace(' ', '.')), expectationData(c.getRow - expectStartRow).getContents)).filter(_._1 != "")
+          println(scenario1Expectations)
+          ""
+      
     }
 
 }
--- src/test/scala/stest/RunnerTest.scala
+++ src/test/scala/stest/RunnerTest.scala
@@ -21,31 +21,14 @@ class RunnerTest extends TestCase {
           val w = Workbook.getWorkbook (st)
           assertNotNull(w)
 
-          doSheet(w.getSheets()(0))
+          val rt = new Runner
+          rt.processSheet(w.getSheets()(0))
+
+          
       }
 
 
-      def doSheet(st: Sheet)  = {
-          val declarationCells = st.getColumn(0).takeWhile((c: Cell) => c.getContents != "WHEN")
-          val dataCells = st getColumn(0) dropWhile(_.getContents != "WHEN") drop(1) takeWhile(_.getContents != "EXPECT")
-          val expectCells = st.getColumn(0).dropWhile(_.getContents != "EXPECT").drop(1)
-
-
-          lazy val dataStartRow = dataCells(0).getRow
-          lazy val expectStartRow = expectCells(0).getRow
-
-          //now lets do column 1
-          val scenarioData = st getColumn(1) dropWhile(_.getRow <  dataStartRow) takeWhile(_.getRow < expectStartRow - 1)
-          val expectationData = st getColumn(1) dropWhile(_.getRow < expectStartRow)
-
-
-          val scenario1Input = dataCells.map((c: Cell) => (c.getContents.replace(' ', '.'), scenarioData(c.getRow - dataStartRow).getContents))
-          println(scenario1Input)
-
-          val scenario1Expectations = expectCells.map((c: Cell) => ((c.getContents.replace(' ', '.')), expectationData(c.getRow - expectStartRow).getContents)).filter(_._1 != "")
-          println(scenario1Expectations)
-          ""
-      }
+    
 
     def testMVEL : Unit = {
 
