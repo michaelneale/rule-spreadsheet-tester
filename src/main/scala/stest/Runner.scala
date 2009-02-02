59402be57c3c2ed022971ac86fde2a19232f6377
Michael Neale <michael.neale@gmail.com>
2009-02-03 09:48:50 +1100

    got some cool stuff with XML literals working, still experimenting

--- src/main/scala/stest/Runner.scala
+++ src/main/scala/stest/Runner.scala
@@ -1,43 +1,54 @@
 package stest
+
 /**
- * 
+ *
  * @author Michael Neale
  */
 
 import java.io.InputStream
 import jxl.{Sheet, Workbook, Cell}
+
 class Runner {
-
-
-
     def processSheet(st: Sheet) = {
-              
-          val declarationCells = st.getColumn(0).takeWhile((c: Cell) => c.getContents != "WHEN")
-          val dataCells = st getColumn(0) dropWhile(_.getContents != "WHEN") drop(1) takeWhile(_.getContents != "EXPECT")
-          val expectCells = st.getColumn(0).dropWhile(_.getContents != "EXPECT").drop(1)
+        val declarationCells = st.getColumn(0).takeWhile(_.getContents != "WHEN")
+        val dataCells = st getColumn (0) dropWhile (_.getContents != "WHEN") drop (1) takeWhile (_.getContents != "EXPECT")
+        val expectCells = st.getColumn(0).dropWhile(_.getContents != "EXPECT").drop(1)
 
 
-          val facts = declarationCells.dropWhile(_.getContents.startsWith("Global"))
-          val globals = declarationCells.takeWhile(_.getContents.startsWith("Global"))
+        val facts = declarationCells.dropWhile(_.getContents.startsWith("Global"))
+        val globals = declarationCells.takeWhile(_.getContents.startsWith("Global"))
+
+        lazy val dataStartRow = dataCells(0).getRow
+        lazy val expectStartRow = expectCells(0).getRow
+
+
+        val allcols = for (i <- 1 to st.getColumns) yield st.getColumn(i)
+        println(allcols(0)(dataStartRow - 1).getContents)
+        //val cols = allcols.filter((col: Array[Cell]) => col(dataStartRow))
+        //println(cols.size)
+
+        //println(cols(0)(dataStartRow - 1).getContents)
 
 
 
+        /*
+        for (i <- 1 to st.getColumns) {
+            val col = st getColumn(i)
+            val scenarioData = col dropWhile (_.getRow < dataStartRow) takeWhile (_.getRow < expectStartRow - 1)
+            val expectationData = col dropWhile (_.getRow < expectStartRow)
+            ""
+            //TODO filter out unneeded cols
+            println(dataCells.map((c: Cell) => (c.getContents.replace(' ', '.'), scenarioData(c.getRow - dataStartRow).getContents)))
+            println(expectCells.map((c: Cell) => ((c.getContents.replace(' ', '.')), expectationData(c.getRow - expectStartRow).getContents)).filter(_._1 != ""))
 
-          lazy val dataStartRow = dataCells(0).getRow
-          lazy val expectStartRow = expectCells(0).getRow
-
-          //now lets do column 1
-          val scenarioData = st getColumn(1) dropWhile(_.getRow <  dataStartRow) takeWhile(_.getRow < expectStartRow - 1)
-          val expectationData = st getColumn(1) dropWhile(_.getRow < expectStartRow)
 
 
-          val scenario1Input = dataCells.map((c: Cell) => (c.getContents.replace(' ', '.'), scenarioData(c.getRow - dataStartRow).getContents))
-          println(scenario1Input)
+        }
+        */
 
-          val scenario1Expectations = expectCells.map((c: Cell) => ((c.getContents.replace(' ', '.')), expectationData(c.getRow - expectStartRow).getContents)).filter(_._1 != "")
-          println(scenario1Expectations)
-          ""
-      
+
+        ""
+
     }
 
 }
--- src/test/scala/stest/RunnerTest.scala
+++ src/test/scala/stest/RunnerTest.scala
@@ -3,7 +3,9 @@ package stest
 import junit.framework._
 import Assert._
 import org.mvel2.MVEL
+import java.util.{HashMap => JavaHash}
 import jxl.{Sheet, Workbook, Cell}
+import scala.xml.XML
 
 /**
  * Remember for all test runners to work, must return Unit !
@@ -33,13 +35,48 @@ class RunnerTest extends TestCase {
     def testMVEL : Unit = {
 
         val f = new SampleFact
-        val hm = new java.util.HashMap[String, Any]
+        val hm = new JavaHash[String, Any]
         hm.put("f",  f)
         assertNotNull(MVEL.eval("f.name='mic'", hm))
         assertNotNull(MVEL.eval("f.age='42'", hm))
         assertEquals("mic", f.name)
         assertEquals(42, f.age)
+
         println("done")
+
+    }
+
+
+    def testAtom : Unit = {
+        val feed = <feed xmlns="http://www.w3.org/2005/Atom">
+                 <title>Example Feed</title>
+                 <subtitle>A subtitle.</subtitle>
+                 <link href="http://example.org/feed/" rel="self"/>
+                 <link href="http://example.org/"/>
+                 <updated>2003-12-13T18:30:02Z</updated>
+                 <author>
+                   <name>John Doe</name>
+                   <email>johndoe@example.com</email>
+                 </author>
+                        { entryItems  }
+                 <id>urn:uuid:60a76c80-d399-11d9-b91C-0003939e0af6</id>
+                </feed>
+
+        
+        println(feed)
+
+    }
+
+    def entryItems = {
+            <entry>
+              <title>Atom-Powered Robots Run Amok</title>
+              <link href="http://example.org/2003/12/13/atom03"/>
+              <id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id>
+              <updated>2003-12-13T18:30:02Z</updated>
+              <summary>Some text.</summary>
+            </entry>
+
+        
     }
 
 
