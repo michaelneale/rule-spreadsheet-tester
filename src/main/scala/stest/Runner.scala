75a206e4507c3edbfb97786e9ebd682f8ac5874f
Michael Neale <michael.neale@gmail.com>
2009-02-04 10:33:36 +1100

    ok got it crawling things correctly

--- src/main/scala/stest/Runner.scala
+++ src/main/scala/stest/Runner.scala
@@ -21,30 +21,23 @@ class Runner {
         lazy val dataStartRow = dataCells(0).getRow
         lazy val expectStartRow = expectCells(0).getRow
 
+        //turn it into a list - thankfully scala is nice and lazy about this
+        val allcols = for (i <- 1 to (st.getColumns - 1)) yield st.getColumn(i)
 
-        val allcols = for (i <- 1 to st.getColumns) yield st.getColumn(i)
-        println(allcols(0)(dataStartRow - 1).getContents)
-        //val cols = allcols.filter((col: Array[Cell]) => col(dataStartRow))
-        //println(cols.size)
-
-        //println(cols(0)(dataStartRow - 1).getContents)
+        //ok here are our scenarios
+        val scenarioColumns = allcols.filter((cs: Array[Cell]) => cs(dataStartRow - 1).getContents != "");
 
 
 
-        /*
-        for (i <- 1 to st.getColumns) {
-            val col = st getColumn(i)
+
+
+
+        for (col <- scenarioColumns) {
             val scenarioData = col dropWhile (_.getRow < dataStartRow) takeWhile (_.getRow < expectStartRow - 1)
             val expectationData = col dropWhile (_.getRow < expectStartRow)
-            ""
-            //TODO filter out unneeded cols
             println(dataCells.map((c: Cell) => (c.getContents.replace(' ', '.'), scenarioData(c.getRow - dataStartRow).getContents)))
             println(expectCells.map((c: Cell) => ((c.getContents.replace(' ', '.')), expectationData(c.getRow - expectStartRow).getContents)).filter(_._1 != ""))
-
-
-
         }
-        */
 
 
         ""
